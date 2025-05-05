634fc9ce5d0b689d1699e7547ee9c5db5d9262d1
JavaNathan <nathanjchin4@gmail.com>
2025-05-05 00:03:48 -0400

    sixteenth version

--- capstone3/src/components/Header.js
+++ capstone3/src/components/Header.js
@@ -27,8 +27,53 @@ function Header(){
                         fontFamily: 'Markazi Text, sans-serif',
                         margin: '0',
                     }}>Little Lemon</h1>
+                    <h3 style={{
+                        color: '#edefee',
+                        fontSize: '2em',
+                        fontFamily: 'Karla, sans-serif',
+                        margin: '0',
+                    }}>Chicago</h3>
+                    <p style={{
+                        color: '#edefee',
+                        fontSize: '1.2em',
+                        fontFamily: 'Karla, sans-serif',
+                    }}>We are a family owned Mediteranian restaurant, 
+                    focused on traditional recipes served with a modern twist.</p>
+                    <button style={{
+                        backgroundColor: '#f4ce14',
+                        border: 'none',
+                        color: 'black',
+                        padding: '15px 32px',
+                        textAlign: 'center',
+                        textDecoration: 'none',
+                        display: 'inline-block',
+                        fontSize: '16px',
+                        margin: '8px 0px',
+                        cursor: 'pointer',
+                        borderRadius: '8px',
+                        fontFamily: 'Karla, sans-serif',
+                        fontWeight: 'bold',
+                    }}
+                    onClick={() => { window.location.href = '/reservations'; }}
+                    >
+                      Reserve a Table
+                    </button>
                 </div>
             </section>
+            <div style={{
+                gridColumn: '8 / span 3',
+                display: 'flex',
+                alignItems: 'top',
+                justifyContent: 'top',
+                margin: '20px 0 -225px 0',
+            }}>
+                <img src={headerImg} alt="Chef serving" style={{
+                    width: '90%',
+                    height: '70%',
+                    objectFit: 'cover',
+                    borderRadius: '10px',
+                }} />
+            </div>
         </div>
     );
 }
