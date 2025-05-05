66c65510d02c39ce1512dc2aeb1a0af619ece6b3
JavaNathan <nathanjchin4@gmail.com>
2025-05-05 07:18:08 -0400

    thirty-fourth version

--- capstone3/src/App.js
+++ capstone3/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from '../capstone3/src/pages/HomePage';
+import BookingPage from '../capstone3/src/pages/BookingPage';
+import LoginPage from '../capstone3/src/pages/LoginPage';
+import ConfirmedBooking from '../capstone3/src/components/ConfirmedBooking';
+import OrderOnlinePage from '../capstone3/src/pages/OrderOnlinePage';
+import '../capstone3/src/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/reservations" element={<BookingPage />} />
+        <Route path="/order" element={<OrderOnlinePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- capstone3/src/App.test.js
+++ capstone3/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import BookingForm from '../capstone3/src/components/BookingForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+test('Renders the BookingForm heading', () => {
+  render(<BookingForm />);
+  const headingElement = screen.getByText("Book a Table");
+  expect(headingElement).toBeInTheDocument();
+})
