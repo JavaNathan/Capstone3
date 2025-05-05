d9898ec46380f9afb0de666a9a6d3e9fe7ef6fc9
JavaNathan <nathanjchin4@gmail.com>
2025-05-05 00:31:50 -0400

    nineteenth version

--- capstone3/src/components/Menu.js
+++ capstone3/src/components/Menu.js
@@ -19,9 +19,36 @@ function Menu(){
                     gridColumn: '3 / span 8',
                 }}>
                     <div className="title" style={{
-
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        padding: '20px',
                     }}>
-                        
+                        <h1 style={{
+                            fontSize: '2.5em',
+                            fontFamily: 'Markazi Text, sans-serif',
+                            margin: '0',
+                        }}>
+                            This weeks specials!
+                        </h1>
+                        <button style={{
+                            backgroundColor: '#f4ce14',
+                            border: 'none',
+                            color: 'black',
+                            padding: '15px 32px',
+                            textAlign: 'center',
+                            textDecoration: 'none',
+                            display: 'inline-block',
+                            fontSize: '16px',
+                            margin: '8px 0px',
+                            cursor: 'pointer',
+                            borderRadius: '8px',
+                            fontFamily: 'Karla, sans-serif',
+                            fontWeight: 'bold',
+                        }}>
+                            Online Menu
+                        </button>
                     </div>
                 </div>
             </div>
