f63d2f2994c89c2644eae7618799ab2b1c6a2d1a
JavaNathan <nathanjchin4@gmail.com>
2025-05-05 01:21:26 -0400

    nineteenth version

--- capstone3/src/components/Menu.js
+++ capstone3/src/components/Menu.js
@@ -50,6 +50,64 @@ function Menu(){
                             Online Menu
                         </button>
                     </div>
+                    <section className="card" style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        gap: '40px',
+                    }}>
+                        <div id="card1" style={{
+                            backgroundColor: '#edefee',
+                            borderRadius: '8px',
+                            width: '256px',
+                            height: 'auto',
+                        }}>
+                            <div>
+                                <img src={greekSalad} alt="greek salad" style={{
+                                    width: '256px',
+                                    height: '192px',
+                                    objectFit: 'cover',
+                                    borderRadius: '8px 8px 0px 0px',
+                                }} />
+                            </div>
+                            <div style={{
+                                flexDirection: 'row',
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                padding: '0 10px',
+                            }}>
+                                <p>Greek Salad</p>
+                                <p>$12.99</p>
+                            </div>
+                            <p style={{ padding: '0 10px', }}>
+                                The famous greek salad of crispy lettuce, cucumbers, tomatoes, onions, feta cheese,
+                                kalamata olives, and pepperoncini peppers.
+                            </p>
+                            <div style={{
+                                flexDirection: 'row',
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                padding: '0 10px',
+                                alignItems: 'center',
+                            }}>
+                                <p>Order a delivery</p>
+                                <img alt="courier" src="https://cdn.iconscout.com/icon/premium/png-256-thumb/delivery-1447237-1223498.png?f=webp&amp;w=256" style={{
+                                    width: '24px',
+                                    height: '24px',
+                                    objectFit: 'cover',
+                                }} />
+                            </div>
+                        </div>
+                        <div id="card2" style={{
+                            backgroundColor: '#edefee',
+                            borderRadius: '8px',
+                            width: '256px',
+                            height: 'auto',
+                        }}>
+                            
+                        </div>
+                    </section>
                 </div>
             </div>
         </>
